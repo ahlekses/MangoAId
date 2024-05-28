4e08f22ac0e94511ea15f87d04ea2792bc645aba
Clepher Jaudian <clepher.jaudian@msunaawan.edu.ph>
2024-05-28 12:24:54 +0800

    Colors and image

--- src/components/LeftBanner.tsx
+++ src/components/LeftBanner.tsx
@@ -5,7 +5,7 @@ import { FadeIn } from "./FadeIn";
 
 const LeftBanner = () => {
   const [text] = useTypewriter({
-    words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
+    words: ["exploring.", "expanding.", "empowering."],
     loop: true,
     typeSpeed: 20,
     deleteSpeed: 10,
@@ -14,13 +14,13 @@ const LeftBanner = () => {
   return (
     <FadeIn className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h4 className=" text-lg font-normal">WELCOME TO</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">John doe</span>
+         Mango<span className="text-designColor capitalize">AI</span>d
         </h1>
         <h2 className="text-4xl font-bold text-white">
-          a <span>{text}</span>
-          <Cursor cursorStyle="|" cursorColor="#ff014f" />
+          join us in <span>{text}</span>
+          <Cursor cursorStyle="|" cursorColor="#AAFF00" />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wider">
           I use animation as a third dimension by which to simplify experiences
